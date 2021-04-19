355722ac73624a4e499c54e90180f3f0dabd9adf
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2021-04-19 12:22:10 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 const Header=(props)=>{
     return(
         <div className="head-container">
-            <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png" alt="App Name Suggestor" className={`head-img ${props.headerExpanded?'head-image-expanded':'head-image-contracted'}`}/>
+            <img src="App-Name-Suggestor/images/" alt="App Name Suggestor" className={`head-img ${props.headerExpanded?'head-image-expanded':'head-image-contracted'}`}/>
             <h1 className="head-text">{props.headTitle}</h1>
         </div>
     )
