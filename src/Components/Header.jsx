4918d0da60db5a646eb96b026347438b03564c79
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2021-04-19 11:40:46 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import Logo from '/images/undraw_Domain_names_re_0uun.svg';
 const Header=(props)=>{
     return(
         <div className="head-container">
-            <img src={Logo} alt="Name Suggestor" className={`head-img ${props.headerExpanded?'head-image-expanded':'head-image-contracted'}`}/>
+            <img src={Logo} alt="App Name Suggestor" className={`head-img ${props.headerExpanded?'head-image-expanded':'head-image-contracted'}`}/>
             <h1 className="head-text">{props.headTitle}</h1>
         </div>
     )
