64a1c726ef80444ac5ea3cbe40d88a645b9085d5
Pramod Reddy <pramodreddy.seri@emc.com>
2021-06-01 15:57:22 +0530

    adding image

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
-
+import logo from "../../images/undraw_Domain_names_re_0uun.svg";
 
 const Header=(props)=>{
     return(
         <div className="head-container">
+	    <img src={logo} alt="Name Suggestor" className={`head-img ${props.headerExpanded?'head-image-expanded':'head-image-contracted'}`}/>
             <h1 className="head-text">{props.headTitle}</h1>
         </div>
     )
