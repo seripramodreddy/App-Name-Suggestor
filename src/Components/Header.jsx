566c3bc3dd095fb225e7c061190e688c38ef155c
Pramod Reddy <pramodreddy.seri@emc.com>
2021-06-01 14:25:30 +0530

    Modified header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,6 @@ import './Header.css';
 const Header=(props)=>{
     return(
         <div className="head-container">
- 
             <h1 className="head-text">{props.headTitle}</h1>
         </div>
     )
