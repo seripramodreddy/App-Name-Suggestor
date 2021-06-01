91eaa147f2c1d50da340903adb95a5994fd97f4d
Pramod Reddy Seri <48101140+seripramodreddy@users.noreply.github.com>
2021-06-01 13:29:06 +0530

    Removing image

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 const Header=(props)=>{
     return(
         <div className="head-container">
-            <img src="./App-Name-Suggestor/images/undraw_Domain_names_re_0uun.svg" alt="Name Suggestor" className={`head-img ${props.headerExpanded?'head-image-expanded':'head-image-contracted'}`}/>
+ 
             <h1 className="head-text">{props.headTitle}</h1>
         </div>
     )
